dc0e30f69b35d25e84ba4462fee197417962f8d9
MARC701 <143324285+MARC701@users.noreply.github.com>
2024-07-21 13:02:10 +0500

    Update antiLink.js

--- plugins/antiLink.js
+++ plugins/antiLink.js
@@ -25,7 +25,7 @@ bot(
 			// 		{ id: 'antilink info', text: 'INFO' },
 			// 		{ id: `antilink ${onOrOff}`, text: onOrOff.toUpperCase() },
 			// 	],
-			// 	'Example\nhttps://github.com/lyfe00011/whatsapp-bot-md/wiki/antilink',
+			// 	'Example\nhttps://www.tiktok.com/@arslanchaudary66',
 			// 	'Antilink'
 			// )
 			// return await message.send(button, {}, 'button')
@@ -35,7 +35,7 @@ bot(
 			// 			{
 			// 				urlButton: {
 			// 					text: 'Example',
-			// 					url: 'https://github.com/lyfe00011/whatsapp-bot-md/wiki/antilink',
+			// 					url: 'https://www.tiktok.com/@arslanchaudary66',
 			// 				},
 			// 			},
 			// 			{
